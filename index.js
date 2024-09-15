79ab6e41afce81b9e0a9774acea320b1b2f1e077
Collin H <cwhouar15@gmail.com>
2024-09-14 18:31:47 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {
+  const data = { name, email };
+
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(function (response) {
+      const contentType = response.headers.get('content-type');
+      if (!contentType) {
+        throw new Error('Response does not contain content-type header');
+      }
+      if (contentType.includes('application/json')) {
+        return response.json();
+      } else {
+        throw new Error('Response not in JSON format');
+      }
+    })
+    .then(function (responseData) {
+      const newId = responseData.id;
+      const domElement = document.createElement('p');
+      domElement.textContent = `User ID: ${newId}`;
+      document.body.appendChild(domElement);
+      console.log(responseData);
+    })
+    .catch(function (error) {
+      const errorElement = document.createElement('p');
+      errorElement.textContent = error.message || 'Unauthorized Access';
+      document.body.appendChild(errorElement);
+      console.log('Fetch Error:', error.message);
+    });
+}
